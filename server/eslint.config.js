169d505416e89a9c3e58c70c6fab87b60f29900e
Chris Ellis <cbe5252@psu.edu>
2026-06-02 20:39:29 -0400

    Add prettier and eslint setup to perform static code analysis and fix code formatting before each commit, and also to run whenever a pull request is opened.

---
+++ server/eslint.config.js
@@ -0,0 +1,25 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        console: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      eqeqeq: 'error'
+    }
+  }
+];
